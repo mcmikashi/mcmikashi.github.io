0162bd86207bb72c3d91b63a6449a7394e9bacda
Anicette Mickaël <mickaelanicette@gmail.com>
2025-05-21 21:03:25 -0300

    Improve layout

---
+++ src/lib/components/layout/Footer.svelte
@@ -0,0 +1,44 @@
+<script>
+	import { Copyright } from '@lucide/svelte';
+	import Github from '../common/icons/Github.svelte';
+	import Linkedin from '../common/icons/Linkedin.svelte';
+	import Twitter from '../common/icons/Twitter.svelte';
+</script>
+
+<footer class="bg-base-100 flex flex-row justify-between items-center px-10 py-4 shadow-sm">
+	<aside class="flex flex-row gap-2">
+		<Copyright />
+		<p>{new Date().getFullYear()} Votre Mickaël Anicette. Tous droits réservés.</p>
+	</aside>
+	<div class="flex flex-row gap-4 items-center">
+		<h6 class="footer-title mb-0">Réseau sociaux</h6>
+		<nav>
+			<div class="grid grid-flow-col gap-8">
+				<a
+					href="https://github.com/mcmikashi"
+					aria-label="Mon profil GitHub - Voir mes projets (nouvel onglet)"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					<Github />
+				</a>
+				<a
+					href="https://www.linkedin.com/in/micka%C3%ABl-anicette-484002245/"
+					aria-label="Visiter mon profil LinkedIn (nouvel onglet)"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					<Linkedin />
+				</a>
+				<a
+					href="https://x.com/mcmikashi973"
+					aria-label="Visiter mon profil Twitter (nouvel onglet)"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					<Twitter/>
+				</a>
+			</div>
+		</nav>
+	</div>
+</footer>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,15 +1,15 @@
 <script lang="ts">
+	import Footer from '$lib/components/layout/Footer.svelte';
 	import Header from '$lib/components/layout/header/Header.svelte';
 	import { theme } from '$lib/store/Theme.svelte';
 	import '../app.css';
 	let { children } = $props();
 </script>
 
-
-<div data-theme={theme.value} class="min-h-screen flex flex-col">
-<Header />
-<main>
-{@render children()}
-</main>
+<div data-theme={theme.value} class="flex min-h-screen flex-col">
+	<Header />
+	<main>
+		{@render children()}
+	</main>
+	<Footer />
 </div>
-
