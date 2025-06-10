e4c8c56da7dbadf9386396770339af38eabe3d76
Anicette Mickaël <mickaelanicette@gmail.com>
2025-06-10 16:28:32 -0300

    Add error fallback page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import { page } from '$app/stores';
+</script>
+
+<div class="mx-auto max-w-page">
+  <h1 class="text-4xl font-bold mt-8">Oups !</h1>
+  <p class="py-4 text-xl">
+    {#if $page.status === 404}
+      Nous avons cherché partout, mais la page que vous demandez est introuvable. Désolé !
+    {:else}
+      Une erreur inattendue s'est produite.
+    {/if}
+  </p>
+</div>
