cb920aafb4aa91954bffe871e840f3a863ed4be6
Anicette Mickaël <mickaelanicette@gmail.com>
2025-06-10 19:13:29 -0300

    Improve error handling by adding console.error

--- src/lib/components/common/Contact.svelte
+++ src/lib/components/common/Contact.svelte
@@ -33,8 +33,8 @@
 					throw new Error('Unknow error');
 				}
 			},
-			// eslint-disable-next-line @typescript-eslint/no-unused-vars
 			function (error) {
+				console.error(error);
 				formResult = false;
 			}
 		);
